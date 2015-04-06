6502f1bf8c7a58de82afcb004d27dff3040d61bd
rohithkaliyur <rohith_kaliyur@yahoo.com>
2015-04-06 16:28:30 -0500

    mickel4

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,7 +1,66 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionListener;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineMetrics;
+import java.awt.geom.Rectangle2D;
 
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 public class FrontEnd {
+	JFrame frame;
+	JPanel panel;
+	JTextField textfield;
+	public FrontEnd()
+	{
+		/*
+		Rectangle2D box;
+		Graphics2D g;
+		String message = "Enter a username";
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		Font f = env.getAllFonts();
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JTextField textfield = new JTextField(15);
+		JLabel label = new JLabel("Enter a valid Username");
+		JButton button = new JButton("Submit");
+		frame.setTitle("Project Unit 6");
+		frame.setVisible(true);
+		frame.setSize(300,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocation(550,200);
+		FontRenderContext frc = g.getFontRenderContext();
+		Rectangle2D bounds = f.getStringBounds(message, frc);
+		LineMetrics metrics = f.getLineMetrics(message, frc);
+		float width = (float) bounds.getWidth();     
+		float lineheight = metrics.getHeight();      
+		float ascent = metrics.getAscent();          
+		float x0 = (float) (box.getX() + (box.getWidth() - width)/2);
+		float y0 = (float) (box.getY() + (box.getHeight() - lineheight)/2 + ascent);
+		g.setFont(f);
+		g.drawString(message, x0, y0);
+		panel.add(label);
+		panel.add(textfield).setSize(100, 100);;
+		panel.add(button);
+		frame.add(panel);
+		
+		
+		
+		
+		
+		*/
+		
+	}
+	
+	
+	
+	
 
-	
-	// rohith
-	
 }
