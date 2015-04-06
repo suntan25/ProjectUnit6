a266ea8e57b06dc3eb53dd540c50193ea848aa71
suntan25 <sunnytian8@gmail.com>
2015-04-06 16:13:41 -0500

    Updates5

--- ProjectUnit6/src/Encoder.java
+++ ProjectUnit6/src/Encoder.java
@@ -1,44 +1,72 @@
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 
 public class Encoder {
 	
-	String line = null;
-	FileReader fileReader;
+	
+	File file;
 	
 	public Encoder(String filename)
 	{
-		try {
-			fileReader =  new FileReader(filename);
-		} catch (FileNotFoundException e) {
-			 System.out.println("Unable to open file '" + filename + "'"); 
-		}
+		file =  new File(filename);
+		
 	}
 	
 	public void write(String plaintext)
 	{
-		
-	}
-	
-	public List<String> read()
-	{	
-		List<String> readIt= new ArrayList<String>();
-		try (BufferedReader br = new BufferedReader(fileReader)) {
-		    String line;
-		    while ((line = br.readLine()) != null) {
-		       readIt.add(line);
-		    }
-		} catch (IOException e) {
+		try {
+			PrintWriter p = new PrintWriter(file);
+			
+			for(String s: myData.keySet()){
+				p.print(s);
+				
+				for(String d: myData.get(s)){
+					p.print("\t");
+					p.print(d);
+				}
+				p.println();
+			}
+			
+			
+			
+			p.close();
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		
+		
+	}
+	
+	
+	public List<String> read()
+	{	ArrayList<String> readIt = new ArrayList<String>();
+		try {
+			Scanner scan = new Scanner(file);
+			
+			while(scan.hasNextLine()){
+				String[] line = scan.nextLine().split("\\t");
+				readIt.add(line[0]);
+				readIt.add(line[1]);			
+			}
+					
+			scan.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return readIt;
 	}
 	
