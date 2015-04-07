098cf56fac4b9079d5b499d98d99f7ff86f25b70
rohithkaliyur <rohith_kaliyur@yahoo.com>
2015-04-07 14:28:50 -0500

    Initial Revision
    Had to take out a ton of lines to get some other functionality to
    compile.

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,13 +1,4 @@
-import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionListener;
-import java.awt.font.FontRenderContext;
-import java.awt.font.LineMetrics;
-import java.awt.geom.Rectangle2D;
-
-import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -20,12 +11,8 @@ public class FrontEnd {
 	JTextField textfield;
 	public FrontEnd()
 	{
-		/*
-		Rectangle2D box;
-		Graphics2D g;
-		String message = "Enter a username";
-		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		Font f = env.getAllFonts();
+		
+		
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JTextField textfield = new JTextField(15);
@@ -36,16 +23,6 @@ public class FrontEnd {
 		frame.setSize(300,400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(550,200);
-		FontRenderContext frc = g.getFontRenderContext();
-		Rectangle2D bounds = f.getStringBounds(message, frc);
-		LineMetrics metrics = f.getLineMetrics(message, frc);
-		float width = (float) bounds.getWidth();     
-		float lineheight = metrics.getHeight();      
-		float ascent = metrics.getAscent();          
-		float x0 = (float) (box.getX() + (box.getWidth() - width)/2);
-		float y0 = (float) (box.getY() + (box.getHeight() - lineheight)/2 + ascent);
-		g.setFont(f);
-		g.drawString(message, x0, y0);
 		panel.add(label);
 		panel.add(textfield).setSize(100, 100);;
 		panel.add(button);
@@ -55,7 +32,7 @@ public class FrontEnd {
 		
 		
 		
-		*/
+		
 		
 	}
 	
